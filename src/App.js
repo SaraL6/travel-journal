19f319021316fb44219ce3b0fff1ca00fe0534f5
SaraL6 <71009666+SaraL6@users.noreply.github.com>
2021-12-27 15:32:27 +0100

    Entry section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import ReactDOM from "react-dom"
 import Entry from "./components/Entry"
 import Navbar from "./components/Navbar"
+import Data from "./data"
 
-export default function App(){
-    return(
-        <div>
-            <Navbar/>
-            <Entry/>
+export default function App() {
+
+    const entries = Data.map(data=>{
+        return  <Entry 
+                    key={data.id}
+                    {...data} />
+    })
+    return (
+        <div >
+            <Navbar  />
+            {entries}
         </div>
     )
 }
 
-ReactDOM.render(<App/>,document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,10 +1,32 @@
 import ReactDOM from "react-dom"
+import TravelPic from "../images/Fuji.jpg"
+import Pin from "../images/Fill 219.png"
 
-export default function Entry(){
-    return(
-        <div></div>
+export default function Entry(props) {
+
+    return (
+        <main>
+            <div class="container">
+                <img src={props.imageUrl} alt="" className="entry--img" />
+                <div className="entry--details">
+                    <div className="entry--location">
+                        <img src={Pin} alt="" className="entry--pin" />
+                        <h4 className="entry--country">{props.location}</h4>
+                        <a href={props.googleMapsUrl} className="entry--maps">View on Google Maps</a>
+                    </div>
+                    <h3 className="entry--title">{props.title}</h3>
+                    <p className="date">{props.startDate} - {props.endDate}</p>
+                    <p>{props.description}</p>
+
+                </div>
+
+            </div>
+            <hr />
+        </main>
+
+
     )
 
 }
 
-ReactDOM.render(<Entry/>,document.getElementById("root"))
+ReactDOM.render(<Entry />, document.getElementById("root"))
